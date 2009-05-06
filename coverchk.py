51e708547b8ec153abadd7840c231440a9ed25c2
Nobuhiro Hayashi <rinrin.ne@gmail.com>
2009-05-07 02:40:42 +0900

    Print cuesheet total

--- coverchk.py
+++ coverchk.py
@@ -4,7 +4,12 @@
 import os
 
 if __name__ == '__main__':
+	cnt =0
+	
 	for root, dirs, files in os.walk("."):
 		if "Image.cue" in files:
+			cnt+=1
 			if not "cover.jpg" in files:
 				print "cover not found: %s" % root
+	
+	print "Cuesheet Total: %d" % cnt
